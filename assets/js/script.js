9a2711169b4c3918e874d9f630ccaa6cb4c3df25
M Abrams <madeline.a.thomas@gmail.com>
2021-10-22 22:15:28 -0400

    Added local storage capability and more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,69 @@
-var timerEl = document.getElementById("time");
-var questionEl = document.getElementById("question");
-var aEl = document.getElementById("a");
-var bEl = document.getElementById("b");
-var cEl = document.getElementById("c");
 var myQuestions = [
   {
     question: "What is JavaScript most commonly used for?",
     answers: [
-      { a: "Making shoes", answer: false },
-      { b: "Making dynamic webpages", answer: true },
-      { c: "Baking cakes", answer: false },
+      { a: "Making shoes", answer: "false" },
+      { b: "Making dynamic webpages", answer: "true" },
+      { c: "Baking cakes", answer: "false" },
     ],
   },
   {
     question: "Which of the following is true about JavaScript?",
     answers: [
-      { a: "It is right-oriented", answer: false },
-      { b: "It is object-oriented", answer: true },
-      { c: "It is not oriented", answer: false },
+      { a: "It is right-oriented", answer: "false" },
+      { b: "It is object-oriented", answer: "true" },
+      { c: "It is not oriented", answer: "false" },
     ],
   },
   {
-    question: "What is 2+2?",
+    question:
+      "Which built-in method returns the character at the specified index?",
     answers: [
-      { a: "2", answer: true },
-      { b: "4", answer: false },
-      { c: "5", answer: false },
+      { a: "characterAt()", answer: "false" },
+      { b: "charAt()", answer: "true" },
+      { c: "getChar()", answer: "false" },
+    ],
+  },
+  {
+    question:
+      "Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+    answers: [
+      { a: "valueOf()", answer: "true" },
+      { b: "toString()", answer: "false" },
+      { c: "toValue()", answer: "false" },
+    ],
+  },
+  {
+    question:
+      "Which of the following function of String object returns the characters in a string between two indexes into the string?",
+    answers: [
+      { a: "slice()", answer: "false" },
+      { b: "split()", answer: "false" },
+      { c: "substring()", answer: "true" },
     ],
   },
 ];
 
+var timerEl = document.querySelector("#time");
+var questionEl = document.querySelector("#question");
+var aEl = document.querySelector("#a");
+var bEl = document.querySelector("#b");
+var cEl = document.querySelector("#c");
+var scoresEl = document.querySelector("#scores");
+
+var i = 0;
+function play() {
+  questionEl.textContent = myQuestions[i].question;
+  aEl.textContent = myQuestions[i].answers[0].a;
+  bEl.textContent = myQuestions[i].answers[1].b;
+  cEl.textContent = myQuestions[i].answers[2].c;
+  aAnswer = myQuestions[i].answers[0].answer;
+  bAnswer = myQuestions[i].answers[1].answer;
+  cAnswer = myQuestions[i].answers[2].answer;
+}
+
 function countdown() {
-  var i = 0;
+  play();
   var timeLeft = 30;
   var timeInterval = setInterval(function () {
     timeLeft--;
@@ -39,86 +71,86 @@ function countdown() {
     if (timeLeft <= 0) {
       clearInterval(timeInterval);
       alert("GAME OVER");
-      currentQuestion = 0;
-      currentA = 0;
-      currentB = 0;
-      currentC = 0;
-      aAnswer = "true";
-      bAnswer = "true";
-      cAnswer = "true";
-      questionEl.textContent = currentQuestion;
-      aEl.textContent = currentA;
-      bEl.textContent = currentB;
-      cEl.textContent = currentC;
       return;
     }
   }, 1000);
-  currentQuestion = myQuestions[i].question;
-  currentA = myQuestions[i].answers[0].a;
-  currentB = myQuestions[i].answers[1].b;
-  currentC = myQuestions[i].answers[2].c;
-  aAnswer = myQuestions[i].answers[0].answer;
-  bAnswer = myQuestions[i].answers[1].answer;
-  cAnswer = myQuestions[i].answers[2].answer;
-  questionEl.textContent = currentQuestion;
-  aEl.textContent = currentA;
-  bEl.textContent = currentB;
-  cEl.textContent = currentC;
 
   aEl.addEventListener("click", function () {
-    if ((aAnswer = "false")) {
-      timeLeft -= 5;
+    if (aAnswer == "false") {
+      if (timeLeft <= 5) {
+        timeLeft = 0;
+        timerEl.textContent = timeLeft + " seconds";
+        return;
+      }
+      timeLeft = timeLeft - 5;
       timerEl.textContent = timeLeft + " seconds";
-      i++;
-      currentQuestion = myQuestions[i].question;
-      currentA = myQuestions[i].answers[0].a;
-      currentB = myQuestions[i].answers[1].b;
-      currentC = myQuestions[i].answers[2].c;
-      aAnswer = myQuestions[i].answers[0].answer;
-      bAnswer = myQuestions[i].answers[1].answer;
-      cAnswer = myQuestions[i].answers[2].answer;
-      questionEl.textContent = currentQuestion;
-      aEl.textContent = currentA;
-      bEl.textContent = currentB;
-      cEl.textContent = currentC;
+    }
+    if (i < myQuestions.length) {
+      i = i + 1;
+      play();
+    } else {
+      var initials = prompt(
+        "Your score is " + timeLeft + "! Please enter your initials"
+      );
+      scoresEl.textContent = "initials:" + initials + "score: " + timeLeft;
+      localStorage.setItem("initials", initials);
+      localStorage.setItem("score", timeLeft);
     }
   });
   bEl.addEventListener("click", function () {
-    if ((bAnswer = "false")) {
-      timeLeft -= 5;
+    if (bAnswer == "false") {
+      if (timeLeft <= 5) {
+        timeLeft = 0;
+        timerEl.textContent = timeLeft + " seconds";
+        return;
+      }
+      timeLeft = timeLeft - 5;
       timerEl.textContent = timeLeft + " seconds";
-      i++;
-      currentQuestion = myQuestions[i].question;
-      currentA = myQuestions[i].answers[0].a;
-      currentB = myQuestions[i].answers[1].b;
-      currentC = myQuestions[i].answers[2].c;
-      aAnswer = myQuestions[i].answers[0].answer;
-      bAnswer = myQuestions[i].answers[1].answer;
-      cAnswer = myQuestions[i].answers[2].answer;
-      questionEl.textContent = currentQuestion;
-      aEl.textContent = currentA;
-      bEl.textContent = currentB;
-      cEl.textContent = currentC;
+    }
+    if (i < myQuestions.length) {
+      i = i + 1;
+      play();
+    } else {
+      var initials = prompt(
+        "Your score is " + timeLeft + "! Please enter your initials"
+      );
+      scoresEl.textContent = "initials:" + initials + "score: " + timeLeft;
+      localStorage.setItem("initials", initials);
+      localStorage.setItem("score", timeLeft);
     }
   });
   cEl.addEventListener("click", function () {
-    if ((cAnswer = "false")) {
-      timeLeft -= 5;
+    if (cAnswer == "false") {
+      if (timeLeft <= 5) {
+        timeLeft = 0;
+        timerEl.textContent = timeLeft + " seconds";
+        return;
+      }
+      timeLeft = timeLeft - 5;
       timerEl.textContent = timeLeft + " seconds";
-      i++;
-      currentQuestion = myQuestions[i].question;
-      currentA = myQuestions[i].answers[0].a;
-      currentB = myQuestions[i].answers[1].b;
-      currentC = myQuestions[i].answers[2].c;
-      aAnswer = myQuestions[i].answers[0].answer;
-      bAnswer = myQuestions[i].answers[1].answer;
-      cAnswer = myQuestions[i].answers[2].answer;
-      questionEl.textContent = currentQuestion;
-      aEl.textContent = currentA;
-      bEl.textContent = currentB;
-      cEl.textContent = currentC;
+    }
+    if (i < myQuestions.length) {
+      i = i + 1;
+      play();
+    } else {
+      var initials = prompt(
+        "Your score is " + timeLeft + "! Please enter your initials"
+      );
+      scoresEl.textContent = "initials:" + initials + "   score: " + timeLeft;
+      localStorage.setItem("initials", initials);
+      localStorage.setItem("score", timeLeft);
     }
   });
 }
+
+var initials = localStorage.getItem("initials");
+var score = localStorage.getItem("score");
+if (initials == null) {
+  initials == "";
+}
+if (score == null) {
+  score == "";
+}
+scoresEl.textContent = "Initials:" + initials + "          Score: " + score;
 
 start.addEventListener("click", countdown);
